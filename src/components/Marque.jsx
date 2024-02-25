3f757112fff960cdc2a53fd83ffe5bf1ac832612
Harsh Bhimate <harshbhimate2018@gmail.com>
2024-02-25 20:53:22 +0530

    added marque (linear animation sentence) (without animation)

---
+++ src/components/Marque.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from "framer-motion"
+function Marque() {
+  return (
+    <div className='w-full py-24 rounded-3xl bg-[#004D43]'>
+      <div className='text border-t-2 border-b-2 border-zinc-400 flex whitespace-nowrap overflow-hidden'>
+        <h1 className='text-[24vw] tracking-tighter font-bold mb-8 uppercase leading-none'>We are Ochi</h1>
+        <h1 className='text-[24vw] tracking-tighter font-bold mb-8 uppercase leading-none'>We are Ochi</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Marque
