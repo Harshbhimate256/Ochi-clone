be464d99c4e1a7320c876f3b6bee7c22ca53b835
Harsh Bhimate <harshbhimate2018@gmail.com>
2024-03-07 00:13:56 +0530

    added cards animation of text floating up on hover

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import {motion} from "framer-motion";
-
+import { Power4 } from "gsap/all";
 function Featured() {
   const [ishovering, sethovering] = useState(false);
-  // onMouseEnter={()=> sethovering(true)} onMouseLeave={()=> sethovering(false)}
+  
   return (
     <div className="w-full py-20">
       <div className="px-16 w-full border-b-[1px] border-zinc-700 pb-20">
@@ -19,16 +19,23 @@ function Featured() {
       </div>
       <div className="px-20">
         <div className="cards flex mt-5 gap-6 w-full">
-          <div className="headings absolute bg-red-500">
-            <h1 className=""></h1>
-          </div>
-          <div  className=" cardContainer relative w-1/2 h-[75vh]">
+          <div onMouseEnter={()=> sethovering(true)} onMouseLeave={()=> sethovering(false)}  className=" cardContainer relative w-1/2 h-[75vh]">
+            <h1 className="absolute right-0 text-[#CDEA68] z-[10] leading-none text-8xl translate-x-1/2 top-1/2 -translate-y-1/2 overflow-hidden">
+              {"FYDE".split("").map((item,index)=>(
+                <motion.span initial={{y:"100%"}} animate={ishovering ? ({y:"0"}) : {y:"100%"}} transition={{ease : Power4.easeInOut , delay: index*.06}} className="inline-block">{item}</motion.span>
+              ))}
+            </h1>
             <div className="card w-full h-full rounded-2xl overflow-hidden">
               <img className="w-full h-full bg-cover" src="../card1 FYDE.png"/>
             </div>
           </div>
           <div  className=" cardContainer relative w-1/2 h-[75vh]">
             <div className="card w-full h-full rounded-2xl overflow-hidden">
+            <h1 className="absolute right-full text-[#CDEA68] z-[10] leading-none text-8xl translate-x-1/2 top-1/2 -translate-y-1/2">
+              {"VISE".split("").map((item,index)=>(
+                <span>{item}</span>
+              ))}
+            </h1>
               <img className="w-full h-full bg-cover" src="../card2 VISE.jpg"/>
             </div>
           </div>
